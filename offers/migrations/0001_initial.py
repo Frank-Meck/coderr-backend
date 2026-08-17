b1a1fa749a66afb6fe56ef3f7e22d935ebf5fd34
Frank-Meck <frankmeckel@ymail.com>
2026-08-17 15:01:17 +0200

    fix: allow unlimited offer revisions

--- offers/migrations/0001_initial.py
+++ offers/migrations/0001_initial.py
@@ -17,28 +17,34 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Offer',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.BigAutoField(auto_created=True,
+                 primary_key=True, serialize=False, verbose_name='ID')),
                 ('title', models.CharField(max_length=255)),
-                ('image', models.ImageField(blank=True, null=True, upload_to='offers/')),
+                ('image', models.ImageField(
+                    blank=True, null=True, upload_to='offers/')),
                 ('description', models.TextField(blank=True, default='')),
                 ('created_at', models.DateTimeField(auto_now_add=True)),
                 ('updated_at', models.DateTimeField(auto_now=True)),
-                ('business', models.ForeignKey(limit_choices_to={'type': 'business'}, on_delete=django.db.models.deletion.CASCADE, related_name='offers', to=settings.AUTH_USER_MODEL)),
+                ('business', models.ForeignKey(limit_choices_to={
+                 'type': 'business'}, on_delete=django.db.models.deletion.CASCADE, related_name='offers', to=settings.AUTH_USER_MODEL)),
             ],
         ),
         migrations.CreateModel(
             name='OfferDetail',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('offer_type', models.CharField(choices=[('basic', 'Basic'), ('standard', 'Standard'), ('premium', 'Premium')], max_length=20)),
+                ('id', models.BigAutoField(auto_created=True,
+                 primary_key=True, serialize=False, verbose_name='ID')),
+                ('offer_type', models.CharField(choices=[
+                 ('basic', 'Basic'), ('standard', 'Standard'), ('premium', 'Premium')], max_length=20)),
                 ('title', models.CharField(max_length=255)),
                 ('price', models.DecimalField(decimal_places=2, max_digits=10)),
                 ('delivery_time_in_days', models.PositiveIntegerField()),
-                ('revisions', models.PositiveIntegerField(default=0)),
+                ('revisions', models.IntegerField(default=0)),
                 ('features', models.JSONField(blank=True, default=dict)),
                 ('created_at', models.DateTimeField(auto_now_add=True)),
                 ('updated_at', models.DateTimeField(auto_now=True)),
-                ('offer', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='details', to='offers.offer')),
+                ('offer', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE,
+                 related_name='details', to='offers.offer')),
             ],
             options={
                 'constraints': [models.UniqueConstraint(fields=('offer', 'offer_type'), name='unique_offer_detail_type')],
